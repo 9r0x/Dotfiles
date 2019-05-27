79538123ac0f2905b11c4ba2b6081ebc22ec4eb6
Shukai Ni <nishukai@hotmail.com>
2019-05-27 10:30:18 +0800

    Init

---
+++ Emacs/init.el
@@ -0,0 +1,111 @@
+;; Startup
+; Increase the garbage collection threshold to ease startup
+(setq gc-cons-threshold (* 200 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 20 1024 1024))))
+
+;; GUI
+; Disable unwanted GUI
+(when window-system
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0)
+  (tooltip-mode 0))
+(setq-default mode-line-format nil
+	      cursor-type 'hbar)
+(blink-cursor-mode -1)
+
+; Startup message
+(setq inhibit-startup-message t
+      initial-scratch-message ""
+      inhibit-splash-screen t
+      user-full-name "Shukai Ni"
+      custom-file "~/.emacs.d/custom.el"
+      window-divider-default-places t
+      window-divider-default-bottom-width 1
+      window-divider-default-right-width 1)
+
+(defun display-startup-echo-area-message ()
+  (message "Oh dear! Oh dear! I shall be too late!"))
+
+(lab-themes-load-style 'light)
+(add-to-list 'default-frame-alist
+             '(ns-transparent-titlebar . t))
+(load custom-file)
+(ac-config-default)
+(ac-flyspell-workaround)
+(add-hook 'after-init-hook 'global-display-line-numbers-mode)
+(add-hook 'after-init-hook 'show-paren-mode)
+(add-hook 'after-init-hook 'savehist-mode)
+(add-hook 'after-init-hook 'global-flycheck-mode)
+
+; Font
+(add-to-list 'default-frame-alist
+             '(font . "Fira Code Retina-14"))
+(load "~/.emacs.d/lisp/fira-code-mode.el")
+(let (
+      ($replacePairs
+       [
+        ["，" ","]
+        ["。" "."]
+        ["；" ";"]
+        ["：" ":"]
+        ["【" "["]
+        ["】" "]"]
+        ["（" "("]
+        ["）" ")"]
+        ["！" "!"]
+        ["、" "\\"]
+        ["／" "/"]
+        ["《" "<"]
+        ["》" ">"]
+        ["‘" "'"]
+        ["’" "'"]
+        ["“" "\""]
+        ["”" "\""]
+        ]
+       ))
+  (mapcar (lambda(x) (define-key key-translation-map
+                       (kbd (elt x 0)) (kbd (elt x 1)))) $replacePairs))
+
+
+;; Behavior
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq ring-bell-function 'ignore)
+(setq ns-right-option-modifier 'option)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying t
+      version-control t
+      delete-old-versions t
+      kept-new-versions 20
+      kept-old-versions 5
+      )
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+
+;; Package
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+
+
+;; Package-related config
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+     (define-key flyspell-mouse-map [mouse-3] #'flyspell-correct-word)))
+
+
+;; Add divider when window no. > 1
+(add-hook 'window-state-change-hook
+	  (lambda () (if (= (count-windows) 1)
+			 (window-divider-mode 0)
+		       (window-divider-mode))))
---
+++ Zsh/.zshrc
@@ -0,0 +1,75 @@
+export ZSH="$HOME/.oh-my-zsh"
+export VISUAL=emacs
+export EDITOR="$VISUAL"
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export DYLD_LIBRARY_PATH=/usr/local/opt/openssl/lib:$DYLD_LIBRARY_PATH
+
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+POWERLEVEL9K_MODE='nerdfont-complete'
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv user dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time status time)
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="\u2570%F{255}\uF460%F{250}\uF460%F{245}\uF460%f "
+POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR="\uE0B4"
+
+plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)
+autoload -U compinit && compinit
+
+source $ZSH/oh-my-zsh.sh
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+PATH="/usr/local/sbin:$PATH"
+#VIRTUALENVWRAPPER_PYTHON=$(which python3)
+source `which virtualenvwrapper.sh`
+
+alias e='emacsclient -t'
+alias top='htop'
+alias eslint='./node_modules/.bin/eslint'
+alias ma='wolframscript'
+
+to_gif () {
+    ffmpeg -y -i $1 -vf fps=10,scale=320:-1:flags=lanczos,palettegen palette.png
+    ffmpeg -i $1 -i palette.png -filter_complex "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+    rm palette.png
+}
+
+p () {
+    export http_proxy=http://127.0.0.1:1087
+    export https_proxy=http://127.0.0.1:1087
+}
+
+pl () {
+    export http_proxy=http://127.0.0.1:56278
+    export https_proxy=http://127.0.0.1:56278
+}
+
+update () {
+    brew upgrade
+    brew cask upgrade
+    upgrade_oh_my_zsh
+}
+
+sys_proxy () {
+    if [ "$1" = "status" ]; then
+	networksetup -getsocksfirewallproxy wi-fi
+    elif [ "$1" = "off" ]; then
+	networksetup -setsocksfirewallproxystate wi-fi off
+	networksetup -getsocksfirewallproxy wi-fi
+    elif [ "$1" = "on" ]; then
+	networksetup -setsocksfirewallproxystate wi-fi on
+	networksetup -setsocksfirewallproxy wi-fi localhost $2
+	networksetup -getsocksfirewallproxy wi-fi
+    fi
+}
+
+testnet (){
+    curl -s https://api.ip.sb/geoip | jq -r '.ip + " " + .country'
+}
+
+# ls -> exa
+# cat -> bat
